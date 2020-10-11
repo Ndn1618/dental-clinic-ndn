9eabc52d5564df4aae5c6a0f7a8f9424a10fb39e
Ndn1618 <ndilbar1618@gmail.com>
2020-10-12 01:26:11 +0500

    Add images of corousel section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var elSiteNav = document.querySelector('.sitenav');
 var elSiteNavToggler = document.querySelector('.sitenav__menu-toggler');
 
+elSiteNav.classList.remove('sitenav--nojs');
+
 elSiteNavToggler.addEventListener('click', function () {
   elSiteNav.classList.toggle('sitenav--open');
 });
