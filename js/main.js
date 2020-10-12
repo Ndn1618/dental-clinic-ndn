c522ba8791a21fd15699720d2814a35c888082fd
Ndn1618 <ndilbar1618@gmail.com>
2020-10-13 02:25:26 +0500

    Add styles when js disabled

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var elSiteNav = document.querySelector('.sitenav');
 var elSiteNavToggler = document.querySelector('.sitenav__menu-toggler');
+var elBody = document.querySelector('.body');
 
 elSiteNav.classList.remove('sitenav--nojs');
+elBody.classList.remove('body--nojs');
 
 elSiteNavToggler.addEventListener('click', function () {
   elSiteNav.classList.toggle('sitenav--open');
